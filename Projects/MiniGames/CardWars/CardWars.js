5caa9670be318f3f3ddac992a49e8e69fd5f3e52
YitongHuGpm20 <43768506+YitongHuGpm20@users.noreply.github.com>
2025-11-11 19:49:03 -0800

    CardWars: added empty deck warning

--- Projects/MiniGames/CardWars/CardWars.js
+++ Projects/MiniGames/CardWars/CardWars.js
@@ -1,3 +1,9 @@
+// ==== References ====
+const playerImg = document.getElementById("player-card");
+const computerImg = document.getElementById("computer-card");
+const resultText = document.getElementById("result-text");
+
+// ==== Variables ====
 let deckId = "";
 let playerScore = 0;
 let computerScore = 0;
@@ -9,6 +15,7 @@ fetch(`https://deckofcardsapi.com/api/deck/new/shuffle/?deck_count=1`)
     deckId = data.deck_id;
   });
 
+// Translate letters to numbers
 const valueMap = {
   ACE: 14,
   KING: 13,
@@ -24,12 +31,17 @@ function drawCards() {
   fetch(`https://deckofcardsapi.com/api/deck/${deckId}/draw/?count=2`)
     .then((res) => res.json())
     .then((data) => {
+      // Check if drew all the cards
+      if (data.remaining <= 0) {
+        document.getElementById("result-text").textContent = "No cards left! Please refresh.";
+        document.querySelector("button").disabled = true;
+        return;
+      }
+
+      // Draw two cards
       const playerCard = data.cards[0];
       const computerCard = data.cards[1];
-
-      const playerImg = document.getElementById("player-card");
-      const computerImg = document.getElementById("computer-card");
-
+      
       //Update card Images
       playerImg.src = playerCard.image;
       computerImg.src = computerCard.image;
@@ -42,8 +54,7 @@ function drawCards() {
         const playerVal = getCardValue(playerCard.value);
         const computerVal = getCardValue(computerCard.value);
 
-        const resultText = document.getElementById("result-text");
-
+        // Get result
         if (playerVal > computerVal) {
           playerScore += 1;
           resultText.textContent = "You Win This Round 🎉";
@@ -53,6 +64,8 @@ function drawCards() {
         } else {
           resultText.textContent = "It's a tie! No Points Awarded";
         }
+        
+        // Print scores
         document.getElementById("player-score").textContent = playerScore;
         document.getElementById("computer-score").textContent = computerScore;
       });
